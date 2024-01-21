55fb853a2c8a9cc566865d624b33abbaeec07495
Edmond <edmond.santiago7@gmail.com>
2024-01-21 15:17:20 +1100

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const form = document.querySelector('#create-task-form')
+    form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildNewTaskDescription(e.target["new-task-description"].value)
+    form.reset();
+  })
 });
+
+function buildNewTaskDescription(newTaskDescription) {
+  const li = document.createElement('li')
+  li.textContent = `${newTaskDescription}  `
+  document.querySelector('#tasks').appendChild(li)
+
+  const btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  li.appendChild(btn);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
